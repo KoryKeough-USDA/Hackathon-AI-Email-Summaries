5d8d2224342c75dd78c64af71c5eca290940a5fc
KoryKeough-USDA <Kory.Keough@usda.gov>
2026-08-10 18:03:42 -0500

    Final

--- hackathon_r_file.R
+++ hackathon_r_file.R
@@ -57,30 +57,61 @@ for (j in 0:days_in_between) {
 
   combined_emails <- ""
   summary_chunk <- ""
-
+  email_text <- ""
+  clean_text <- ""
+  
   # Flagging emails with keywords found in filter
   if (this_days_emails$Count() > 0) {
     for (i in 1:this_days_emails$Count()) {
       item <- this_days_emails$Item(i)
       
       get_message_text <- function(item){
-        html_content <- tryCatch(item$HTMLBody(), error = function(e) NULL)
-        if (!is.null(html_content)) { # HTML format
-          text_content <- tryCatch({
-            xml_text(read_html(html_content))
-            }, error = function(e) NULL)
-          return(text_content)
-         } else { # Plain text format
-          tryCatch(item$Body(), error = function(e) NULL)
+        body_format <- tryCatch(item$BodyFormat(), error = function(e) NULL)
+        if(body_format == 0 | body_format == 3) {
+          # Force Outlook to convert unidentified and RTF formats to HTML
+          item[["BodyFormat"]] <- 2
+          body_format <- 2
+          
         }
+        if(body_format == 2) {
+          email_text <- tryCatch(item$HTMLBody(), error = function(e) "")
+          if (!is.null(email_text) && nzchar(trimws(email_text))) {
+            clean_text <- tryCatch({
+              email_text %>%
+                read_html() %>%
+                html_text2()
+            }, error = function(e) "")
+        } else if (!nzchar(trimws(email_text))) {
+              clean_text <- tryCatch({
+                inspector <- item$GetInspector()
+                word_doc <- inspector$WordEditor()
+                word_doc[["Content"]][["Text"]]
+              }, error = function(e) "")
+              if (clean_text == "") {
+                clean_text <- tryCatch({
+                pa <- item$PropertyAccessor()
+                pa$GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001E")
+              }, error = function(e) "")
+              }
+        }
+      } else if (body_format == 1) {
+        # Plain text format
+        clean_text <- tryCatch({
+          item$Body()
+        }, error = function(e) "")
+      } else {
+          return("No readable text content found in email body")
       }
-      combined_emails <- paste(combined_emails, "### START OF EMAIL (File: ", item$Subject(), ")\n", get_message_text(item), "\n### END OF EMAIL\n---\n", collapse = "\n")
-    }
-    message("Sending email batch to Groq")
-    # chat_github was discontinued on July 30, 2026
-    # Set up GROQ_API_KEY in .Renviron and sign up on the Groq website using your email address before proceeding
-    chat <- chat_groq(model = "llama-3.3-70b-versatile")
-    summary_chunk <- chat$chat(paste0("Summarize the following emails and explicitly tell me if any actions need to be taken:\n\n", combined_emails))
+    return(clean_text)
+      }
+  complete_email <- paste("### START OF EMAIL (File: ", item$Subject(), ")\n", get_message_text(item), "\n### END OF EMAIL\n---\n", collapse = "\n")
+  message("Sending complete email to Groq")
+  # chat_github was discontinued on July 30, 2026
+  # Set up GROQ_API_KEY in .Renviron and sign up on the Groq website using your email address before proceeding
+  chat <- chat_groq(model = "llama-3.3-70b-versatile", params = params(max_tokens = 2500))
+  summary_chunk <- chat$chat(paste0("Summarize the following emails, placing the date ", format(end_date - j, "%m/%d/%Y"), " on top of the summary, and explicitly tell me if any actions need to be taken:\n\n", complete_email))
+    } 
+    Sys.sleep(10)
   } else {
     message("No emails to summarize this day.")
   }
--- hackathon_r_file_scheduler.R
+++ hackathon_r_file_scheduler.R
@@ -12,15 +12,16 @@ library(lubridate)
 library(curl)
 library(ellmer)
 library(xml2)
+library(rvest)
 
-if(file.exists("scheduler_config.rds")){
+if(file.exists(paste0(dirname(Sys.getenv("HACKATHON_R_SCRIPT_PATH")), "/scheduler_config.rds"))){
   message("Loading scheduler_config.rds")
-  scheduler_config <- readRDS("scheduler_config.rds")
+  scheduler_config <- readRDS(paste0(dirname(Sys.getenv("HACKATHON_R_SCRIPT_PATH")), "/scheduler_config.rds"))
   sharedEmail <- scheduler_config$sharedEmail
-  folderType <- scheduler_config$folderType
+  folderType <- scheduler_config[[2]]
   freq <- scheduler_config$freq
   start_taskscheduler_date <- scheduler_config$start_taskscheduler_date
-  folder_dir <- scheduler_config$folder_dir
+  folder_dir <- scheduler_config[[3]]
 } else {
   message("scheduler_config.rds not found. Please run the hackathon_shiny_app.R script to create the configuration file.")
 }
@@ -34,7 +35,7 @@ recipient_scheduler <- outlookNameSpace_scheduler$CreateRecipient(sharedEmail)
 recipient_scheduler$Resolve()
 
 # Set the inbox_scheduler variable to "outlookNameSpace_scheduler$GetDefaultFolder(6)" whenever you want to summarize emails from your own inbox.
-inbox_scheduler <- outlookNameSpace_scheduler$GetSharedDefaultFolder(recipient_scheduler, folderType)
+inbox_scheduler <- outlookNameSpace_scheduler$GetSharedDefaultFolder(recipient_scheduler, 6)
 if(folderType == "Inbox"){
 messages_scheduler <- inbox_scheduler$Items()
 } else if(folderType == "Archives"){
@@ -72,18 +73,58 @@ for (j in 0:days_in_between_scheduler) {
   # Flagging emails with keywords found in filter
   if (this_days_emails_scheduler$Count() > 0) {
     for (i in 1:this_days_emails_scheduler$Count()) {
-      item <- this_days_emails_scheduler$Item(i)
-      combined_emails_scheduler <- paste(combined_emails_scheduler, "### START OF EMAIL (File: ", item$Subject(), ")\n", item$Body(), "\n### END OF EMAIL\n---\n", collapse = "\n")
+      item_scheduler <- this_days_emails_scheduler$Item(i)
+      
+      get_message_text_scheduler <- function(item_scheduler){
+        body_format_scheduler <- tryCatch(item_scheduler$BodyFormat(), error = function(e) NULL)
+        if(body_format_scheduler == 0 | body_format_scheduler == 3) {
+          # Force Outlook to convert unidentified and RTF formats to HTML
+          item_scheduler[["BodyFormat"]] <- 2
+          body_format_scheduler <- 2
+          
+        }
+        if(body_format_scheduler == 2) {
+          email_text_scheduler <- tryCatch(item_scheduler$HTMLBody(), error = function(e) "")
+          if (!is.null(email_text_scheduler) && nzchar(trimws(email_text_scheduler))) {
+            clean_text_scheduler <- tryCatch({
+              email_text_scheduler %>%
+                read_html() %>%
+                html_text2()
+            }, error = function(e) "")
+          } else if (!nzchar(trimws(email_text_scheduler))) {
+            clean_text_scheduler <- tryCatch({
+              inspector_scheduler <- item_scheduler$GetInspector()
+              word_doc_scheduler <- inspector_scheduler$WordEditor()
+              word_doc_scheduler[["Content"]][["Text"]]
+            }, error = function(e) "")
+            if (clean_text_scheduler == "") {
+              clean_text_scheduler <- tryCatch({
+                pa_scheduler <- item_scheduler$PropertyAccessor()
+                pa_scheduler$GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001E")
+              }, error = function(e) "")
+            }
+          }
+        } else if (body_format_scheduler == 1) {
+          # Plain text format
+          clean_text_scheduler <- tryCatch({
+            item_scheduler$Body()
+          }, error = function(e) "")
+        } else {
+          return("No readable text content found in email body")
+        }
+        return(clean_text_scheduler)
+      }
+      complete_email_scheduler <- paste("### START OF EMAIL (File: ", item_scheduler$Subject(), ")\n", get_message_text_scheduler(item_scheduler), "\n### END OF EMAIL\n---\n", collapse = "\n")
     }
-    message("Sending email batch to Groq")
+    message("Sending complete email to Groq")
     # chat_github was discontinued on July 30, 2026
     # Set up GROQ_API_KEY in .Renviron and sign up on the Groq website using your email address before proceeding
-    chat_scheduler <- chat_groq(model = "llama-3.3-70b-versatile")
-    summary_chunk_scheduler <- chat_scheduler$chat(paste0("Summarize the following emails and explicitly tell me if any actions need to be taken:\n\n", combined_emails_scheduler))
-    total_summary_scheduler <- c(total_summary_scheduler, summary_chunk_scheduler)
+    chat_scheduler <- chat_groq(model = "llama-3.3-70b-versatile", params = params(max_tokens = 2500))
+    summary_chunk_scheduler <- chat_scheduler$chat(paste0("Summarize the following emails, placing the date ", format(Sys.Date() - j, "%m/%d/%Y"), " on top of the summary, and explicitly tell me if any actions need to be taken:\n\n", complete_email_scheduler))
   } else {
     message("No emails to summarize this day.")
   }
+  total_summary_scheduler <- c(total_summary_scheduler, summary_chunk_scheduler)
 }
     writeLines(total_summary_scheduler, paste0(folder_dir, "/Groq_email_summary", todays_date, ".txt"))
     return(total_summary_scheduler)
